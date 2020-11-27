6a5d69b2a76ef0e70997075839581e66d64b7ea9
Arttu Ylhävuori <10089872+areee@users.noreply.github.com>
2020-11-27 16:03:17 +0200

    5.7 ready (show additional information; had to do for exercise 5.14)

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const buttonText = visible ? 'hide' : 'view'
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>{buttonText}</button>
+      </div>
+      <div style={showWhenVisible}>
+        {blog.url} <br />
+        likes {blog.likes} <button>like</button> <br />
+        {blog.user.name}
+      </div>
+    </div>
+  )
+}
 
 Blog.propTypes = {
   blog: PropTypes.object.isRequired,
