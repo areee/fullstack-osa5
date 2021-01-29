73ed6eb9939d3e8f070189616d297a5081126d0d
Arttu Ylhävuori <10089872+areee@users.noreply.github.com>
2021-01-29 15:42:28 +0200

    exercise 5.14 ready

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,40 +1,59 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 // import { prettyDOM } from '@testing-library/dom'
 import Blog from './Blog'
 
-test('renders title and author but not url and likes', () => {
-  const blog = {
-    title: 'Hienonhieno testiotsikko johonkin supermahtavaan blogiin',
-    author: 'Testi T. Testaaja',
-    url: 'http://www.testiosoite.fi',
-    likes: 0,
-    user: { name: 'Arttu' },
-  }
+describe('<Blog />', () => {
+  let component
 
-  const component = render(<Blog blog={blog} />)
+  beforeEach(() => {
+    const blog = {
+      title: 'Hienonhieno testiotsikko johonkin supermahtavaan blogiin',
+      author: 'Testi T. Testaaja',
+      url: 'http://www.testiosoite.fi',
+      likes: 0,
+      user: { name: 'Arttu' },
+    }
 
-  // component.debug()
+    component = render(<Blog blog={blog} />)
+  })
 
-  // const debugDiv = component.container.querySelector('div')
-  // console.log(prettyDOM(debugDiv))
+  test('renders title and author but not url and likes', () => {
+    const div = component.container.querySelector('.blog')
+    expect(div).toHaveTextContent(
+      'Hienonhieno testiotsikko johonkin supermahtavaan blogiin'
+    )
+    expect(div).toHaveTextContent('Testi T. Testaaja')
 
-  // expect(component.container).toHaveTextContent(
-  //   'Hienonhieno testiotsikko johonkin supermahtavaan blogiin'
-  // )
+    const anotherDiv = component.container.querySelector('.togglableContent')
+    expect(anotherDiv).toHaveStyle('display: none')
+  })
 
-  // const element = component.getByText(
-  //   'Component testing is done with react-testing-library'
-  // )
-  // expect(element).toBeDefined()
+  test('url and likes are showed when view button is clicked', () => {
+    // component.debug()
 
-  const div = component.container.querySelector('.blog')
-  expect(div).toHaveTextContent(
-    'Hienonhieno testiotsikko johonkin supermahtavaan blogiin'
-  )
-  expect(div).toHaveTextContent('Testi T. Testaaja')
+    // const debugDiv = component.container.querySelector('div')
+    // console.log(prettyDOM(debugDiv))
 
-  const anotherDiv = component.container.querySelector('.togglableContent')
-  expect(anotherDiv).toHaveStyle('display: none')
+    // expect(component.container).toHaveTextContent(
+    //   'Hienonhieno testiotsikko johonkin supermahtavaan blogiin'
+    // )
+
+    // const element = component.getByText(
+    //   'Component testing is done with react-testing-library'
+    // )
+    // expect(element).toBeDefined()
+
+    const button = component.getByText('view')
+    fireEvent.click(button)
+
+    // component.debug()
+
+    const div = component.container.querySelector('.togglableContent')
+    expect(div).not.toHaveStyle('display: none')
+
+    expect(div).toHaveTextContent('http://www.testiosoite.fi')
+    expect(div).toHaveTextContent('likes 0')
+  })
 })
