7e76d07e73eb47614a69311cff75b6935bff1b59
areee <10089872+areee@users.noreply.github.com>
2021-01-30 12:14:15 +0200

    the 1st version for 5.18

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      username: 'areee',
+      password: 'salainen',
+      name: 'Arttu Ylhävuori',
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -12,5 +18,23 @@ describe('Blog app', function () {
     cy.contains('username')
     cy.contains('password')
     cy.contains('login')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('areee')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+
+      cy.contains('Arttu Ylhävuori logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('areee')
+      cy.get('#password').type('vaarin')
+      cy.get('#login-button').click()
+
+      cy.get('html').should('not.contain', 'Arttu Ylhävuori logged in')
+    })
   })
 })
